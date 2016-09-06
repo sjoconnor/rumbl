0e8f44c2aff80c757dedaccbb0cbfa005f8b8485
Shane O'Connor <sjo152@gmail.com>
2016-09-06 19:37:46 -0400

    In the middle of adding sessions

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,6 +1,8 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
 
+  plug :authenicate when action in [:index, :show]
+
   def index(conn, _params) do
     users = Repo.all(Rumbl.User)
     render conn, "index.html", users: users
@@ -9,5 +11,36 @@ defmodule Rumbl.UserController do
   def show(conn, %{"id" => id}) do
     user = Repo.get(Rumbl.User, id)
     render conn, "show.html", user: user
+  end
+
+  alias Rumbl.User
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.registration_changeset(%User{}, user_params)
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> Rumbl.Auth.login(user)
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp authenicate(conn, opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to acces that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
   end
 end
